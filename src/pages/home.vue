98f156ec49fd08f0b4a77d5589cb229af5e46718
chanhyoung <chlee3927@gmail.com>
2025-04-18 18:05:13 +0900

    unplugin-vue-router 추가

---
+++ src/pages/home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="texst-h4">Home</div>
+    <div>{{ $route.name }}</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="text-h4">Index Vue</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
