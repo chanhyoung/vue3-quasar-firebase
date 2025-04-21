fe95e7b18d72d9b5f32a385a103142feba5290ea
chanhyoung <chlee3927@gmail.com>
2025-04-21 17:08:03 +0900

    레이아웃 3단 분리

---
+++ src/pages/components/PostLeftBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <StickySiceBar>
+    <q-card flat bordered>
+      <q-list bordered separator>
+        <q-item clickable v-ripple :active="true">
+          <q-item-section>전체</q-item-section>
+        </q-item>
+        <q-item v-for="cate in categories" :key="cate.value" clickable v-ripple>
+          <q-item-section>{{ cate.label }}</q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+  </StickySiceBar>
+</template>
+
+<script setup>
+import StickySiceBar from 'src/components/StickySideBar.vue';
+
+const categories = [
+  { label: 'Q&A', value: 'qna' },
+  { label: '커뮤니티', value: 'community' },
+  { label: '공지사항', value: 'notice' },
+  { label: '강의', value: 'lecture' },
+];
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,22 @@
 <template>
   <q-page padding>
-    <div class="text-h4">커뮤니티 목록</div>
-    <section class="q-gutter-y-sm q-mt-lg">
-      <PostList :items="posts" />
-    </section>
+    <div class="row q-col-gutter-x-lg">
+      <PostLeftBar class="col-grow"></PostLeftBar>
+      <section class="col-7">
+        <PostHeader></PostHeader>
+        <PostList :items="posts" />
+      </section>
+      <PostRightBar class="col-3"></PostRightBar>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import PostList from 'src/components/apps/post/PostList.vue';
+import PostHeader from 'src/pages/components/PostHeader.vue';
+import PostLeftBar from 'src/pages/components/PostLeftBar.vue';
+import PostRightBar from 'src/pages/components/PostRightBar.vue';
+
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
